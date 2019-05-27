bdb45041c7ab110e8476b65b41cc62e0708fa957
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-05-26 22:15:33 -0700

    continue working on styling of REET calculator

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from './realogics-blue.jpg';
 
 const Header = props => {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} alt="Realogics Logo" />
           <h1>
-            REET Calculator
+            Real Estate Excise Tax Calculator
           </h1>
         </header>
       </div>
