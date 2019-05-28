3bbc3316e0c37e2338a41feea67ae6c694873b84
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-05-28 15:08:16 -0700

    added additional information to the footer

--- my-app/src/components/footer/footer.js
+++ my-app/src/components/footer/footer.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Footer = props => {
     return (
         <footer>
+            <h3>If you’re looking for a local real estate professional with a global reach and an award-winning marketing team, call us today:</h3>
+            <h3><a href="https://www.rsir.com/eng" target="_blank" rel="noopener noreferrer">RSIR.com</a> | <a href="tel:2065980730" target="_blank" rel="noopener noreferrer">206.598.0730</a></h3>
             <h4>This information is deemed reliable but cannot be guaranteed. Consult a qualified professional before making any investment decision. All excise tax calculations are rounded to the nearest dollar.</h4>
         </footer>
     );
