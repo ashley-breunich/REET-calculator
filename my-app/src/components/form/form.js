29def39ce3d6c81b41cf34440d9edb8d1090ea68
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-05-29 09:37:19 -0700

    change intro text

--- my-app/src/components/form/form.js
+++ my-app/src/components/form/form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = props => {
     return (
         <form className='form'>
-             <p>Effective January 1st, 2020, Washington state will begin calculating real estate excise taxes (REET) based on a tier system. Find out how this could effect your plans for buying or selling a home.</p>
+             <p>Effective January 1st, 2020, Washington state will begin calculating real estate excise taxes (REET) based on a tier system. Find out how this could effect your plans for selling your home.</p>
             <h2>How much is your home worth?</h2>
             <input onChange={props.handleInput} />
             <button onClick={props.handleSubmit}>Calculate</button>
