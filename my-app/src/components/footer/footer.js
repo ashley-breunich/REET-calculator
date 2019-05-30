68a0fe4135140c008817d32c5f60ca2c7f0f8ae6
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-05-30 14:06:06 -0700

    edits from Eddie and Stacy

--- my-app/src/components/footer/footer.js
+++ my-app/src/components/footer/footer.js
@@ -4,7 +4,7 @@ const Footer = props => {
     return (
         <footer>
             <h3>If you’re looking for a local real estate professional with a global reach and an award-winning marketing team, call us today:</h3>
-            <h3><a href="https://www.rsir.com/eng" target="_blank" rel="noopener noreferrer">RSIR.com</a> | <a href="tel:2065980730" target="_blank" rel="noopener noreferrer">206.598.0730</a></h3>
+            <h3><a href="https://www.rsir.com/eng" target="_blank" rel="noopener noreferrer">RSIR.com</a> | <a href="tel:2064485752" target="_blank" rel="noopener noreferrer">206.448.5752</a></h3>
             <h4>This information is deemed reliable but cannot be guaranteed. Consult a qualified professional before making any investment decision. All excise tax calculations are rounded to the nearest dollar.</h4>
         </footer>
     );
--- my-app/src/components/form/form.js
+++ my-app/src/components/form/form.js
@@ -4,7 +4,7 @@ const Form = props => {
     return (
         <form className='form'>
              <p>Effective January 1st, 2020, Washington state will begin calculating real estate excise taxes (REET) based on a tier system. Find out how this could effect your plans for selling your home.</p>
-            <h2>How much is your home worth?</h2>
+            <h2>Enter Your Home Value to Calculate REET Tax:</h2>
             <input onChange={props.handleInput} />
             <button onClick={props.handleSubmit}>Calculate</button>
         </form>
