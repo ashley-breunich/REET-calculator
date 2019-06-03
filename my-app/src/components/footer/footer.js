b278d863d563a5de00134bd74412d23926b35d67
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-06-03 15:58:14 -0700

    edits to disclaimers

--- my-app/src/components/footer/footer.js
+++ my-app/src/components/footer/footer.js
@@ -5,7 +5,7 @@ const Footer = props => {
         <footer>
             <h3>If you’re looking for a local real estate professional with a global reach and an award-winning marketing team, call us today:</h3>
             <h3><a href="https://www.rsir.com/eng" target="_blank" rel="noopener noreferrer">RSIR.com</a> | <a href="tel:2064485752" target="_blank" rel="noopener noreferrer">206.448.5752</a></h3>
-            <h4>This information is deemed reliable but cannot be guaranteed. Consult a qualified professional before making any investment decision. All excise tax calculations are rounded to the nearest dollar.</h4>
+            <h4>This information is deemed reliable but cannot be guaranteed. Consult a qualified professional before making any investment decision. All excise tax calculations are rounded to the nearest dollar. Sotheby's International Realty® is a registered trademark licensed to Sotheby’s International Realty Affiliates LLC.  Each Office Is Independently Owned And Operated. Seller reserves the right to change product offering without notice.</h4>
         </footer>
     );
   }
